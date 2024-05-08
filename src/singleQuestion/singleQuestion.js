301153b4c81367ba69e1ea6fe9d312f1c23a85ed
KacperOldz <kobula@protonmail.com>
2024-05-08 16:40:19 +0200

    Edited singleQuestion component in order to make questions not reoccurence, cosmetic changes

--- src/singleQuestion/singleQuestion.js
+++ src/singleQuestion/singleQuestion.js
@@ -5,20 +5,30 @@ import { RadioGroup, Radio, FormControlLabel } from '@mui/material';
 import "./singleQuestion.css"
 
 function SingleQuestion({ allQuestions }) {
+    const [allQuestionsShuffled, setAllQuestionsShuffled] = useState(null)
     const [singleQuestion, setSingleQuestion] = useState(null);
     const [selectedValue, setSelectedValue] = useState(null);
     const [isRight, setIsRight] = useState(null);
+    const [currentIndex, setCurrentIndex] = useState(0);
 
     useEffect(() => {
-        getRandomQuestion();
-    }, []);
+        const shuffledData = allQuestions.sort(() => Math.random() - 0.5);
+        setAllQuestionsShuffled(shuffledData);
+    }, [allQuestions]);
 
-    const getRandomQuestion = () => {
+    useEffect(() => {
+        if (allQuestionsShuffled !== null) {
+            setSingleQuestion(allQuestionsShuffled[currentIndex]);
+        }
+    }, [allQuestionsShuffled, currentIndex]);
+
+    const getNextQuestion = () => {
         setIsRight(null);
         setSelectedValue(null);
         if (allQuestions && allQuestions.length > 0) {
-            const randomIndex = Math.floor(Math.random() * allQuestions.length);
-            setSingleQuestion(allQuestions[randomIndex]);
+            const newIndex = currentIndex + 1
+            setCurrentIndex(newIndex)
+            setSingleQuestion(allQuestionsShuffled[currentIndex]);
         }
     }
 
@@ -35,8 +45,8 @@ function SingleQuestion({ allQuestions }) {
         <div id="right-panel" style={{ padding: "4%", color: "rgb(63, 63, 63)", width: "100%", height: "100%" }}>
             <div style={{height: "10vh"}}>
                 <h1>{isRight === null ? "Select Answer" : isRight === true ? <span style={{color: "green"}}>Correct Answer!</span> : <span style={{color: "red"}}>Incorrect Answer!</span>}</h1>
-                <Button variant="contained" onClick={getRandomQuestion}>
-                        Another one
+                <Button variant="contained" onClick={getNextQuestion}>
+                        Next one ({currentIndex + 1}/{ allQuestions.length})
                 </Button>
             </div>
             {singleQuestion && (
